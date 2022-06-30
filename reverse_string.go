871560abda48c30296997c3ce3cd8edd82feef38
Starms-go <evra3487@gmainl.com>
2022-06-30 23:25:45 +0300

    first attempt

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	r := []rune(input)
+	for i := len(r) - 1; i >= 0; i-- {
+		output += string(input[i])
+	}
 	return output
 }
